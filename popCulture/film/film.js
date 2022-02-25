462fb1d9fd9e59a9efbb6bdb5805e4554f52e104
IBBC78 <99761517+IBBC78@users.noreply.github.com>
2022-02-25 13:42:57 +0100

    Add files via upload

---
+++ popCulture/film/film.js
@@ -0,0 +1,38 @@
+function MARVEL() {
+    document.getElementById("paragraphe").innerHTML = "L'univers cinématographique Marvel (en anglais : Marvel Cinematic Universe, parfois abrégé en MCU) est une franchise cinématographique produite par Marvel Studios mettant en scène des personnages de bandes dessinées de l'éditeur Marvel Comics, imaginée et mise en route par Kevin Feige à partir de 2008. Marvel Studios est la propriété de The Walt Disney Company. Les films qui font partie de cet univers partagé sont, à l'origine, ceux mettant en vedette Les Avengers, un groupe de super-héros comprenant Iron Man, Hulk, Thor, Black Widow, Hawkeye et Captain America. Sorti en 2008, c'est le film Iron Man qui lance cet univers et donc, la première phase, qui se terminera par la sortie d'Avengers en 20121. La seconde phase est initiée par la sortie de Iron Man 3 en 2013 et s'est conclue avec Ant-Man en 2015. La troisième phase, quant à elle, a débuté lors de la sortie de Captain America: Civil War en 2016, et s'est achevée en 2019 avec la sortie de Spider-Man: Far From Home. Les trois premières phases de cet univers forment un arc narratif connu sous le nom de The Infinity Saga.";
+
+    var e = document.getElementById('paragraphe');
+    e.style.color = 'white';
+    e.style.padding = '30px';
+    e.style.textAlign = 'justify';
+    e.style.fontSize = '25px';
+}
+function DC() {
+    document.getElementById("paragraphe").innerHTML = "L'univers cinématographique DC (souvent désigné en anglais sous l'intitulé officiel DC Extended Universe, abrégé en DCEU.1) est une franchise cinématographique produite par DC Entertainment puis par DC Films et distribués par Warner Bros., mettant en scène un univers étendu avec des super-héros de bandes dessinées de l'éditeur DC Comics. À ce jour, 10 films sont sortis : Man of Steel (2013), Batman v Superman : L'Aube de la justice, Suicide Squad (2016), Wonder Woman, Justice League (2017), Aquaman (2018), Shazam! (2019), Birds of Prey (et la fantabuleuse histoire de Harley Quinn) (2020), Wonder Woman 1984 (2020) et enfin The Suicide Squad (2021). L'univers s'est également étendu avec Zack Snyder's Justice League (2021), version director's cut de Justice League et film hors-série. D'autres projets sont par ailleurs en cours de développement, dont la première série télévisée de l'univers, Peacemaker.";
+
+    var e = document.getElementById('paragraphe');
+    e.style.color = 'white';
+    e.style.padding = '30px';
+    e.style.textAlign = 'justify';
+    e.style.fontSize = '25px';
+}
+function SW() {
+    document.getElementById("paragraphe").innerHTML = "Star Wars est un univers de science-fiction créé par George Lucas. D'abord conçue comme une trilogie cinématographique sortie entre 1977 et 1983, la saga s'accroît ensuite, entre 1999 et 2005, de trois nouveaux films, qui racontent des événements antérieurs à la première trilogie. Cette dernière (épisodes IV, V et VI) ainsi que la deuxième trilogie dite « Prélogie » (épisodes I, II et III) connaissent un immense succès commercial et un accueil critique généralement positif. Dans un souci de cohérence, et pour atteindre un résultat qu'il n'avait pas pu obtenir dès le départ, le créateur de la saga retravaille également les films de sa première trilogie, ressortis en 1997 et 2004 dans de nouvelles versions.";
+
+    var e = document.getElementById('paragraphe');
+    e.style.color = 'white';
+    e.style.padding = '30px';
+    e.style.textAlign = 'justify';
+    e.style.fontSize = '25px';
+}
+function CINEMA() {
+    document.getElementById("paragraphe").innerHTML = "Le cinéma est un art du spectacle. En français, il est désigné comme le « septième art », d'après l'expression du critique Ricciotto Canudo dans les années 19201. L’art cinématographique se caractérise par le spectacle proposé au public sous la forme d’un film, c’est-à-dire d’un récit (fictionnel ou documentaire), véhiculé par un support (pellicule souple, bande magnétique, contenant numérique) qui est enregistré puis lu par un mécanisme continu ou intermittent qui crée l’illusion d’images en mouvement, ou par un enregistrement et une lecture continus de données informatiques. La communication au public du spectacle enregistré, qui se différencie ainsi du spectacle vivant, se fait à l’origine par l’éclairement à travers le support, le passage de la lumière par un jeu de miroirs ou/et des lentilles optiques, et la projection de ce faisceau lumineux sur un écran transparent (Émile Reynaud, Thomas Edison) ou opaque (Louis Lumière), ou la diffusion du signal numérique sur un écran plasma ou à led. Au sens originel et limitatif, le cinéma est la projection en public d’un film sur un écran (en salle ou en plein-air). Dès Émile Reynaud, en 1892, les créateurs de films comprennent que le spectacle projeté gagne à être accompagné par une musique qui construit l’ambiance du récit, ou souligne chaque action représentée. Très rapidement, ils ajoutent des bruits provoqués par un assistant à l’occasion de chaque projection, et font commenter les actions par un bonimenteur présent lui aussi dans la salle. Depuis son invention, le cinéma est devenu à la fois un art populaire, un divertissement, une industrie et un média. Il peut aussi être utilisé à des fins publicitaires, de propagande, de pédagogie ou de recherche scientifique ou relever d'une pratique artistique personnelle et singulière.";
+
+    var e = document.getElementById('paragraphe');
+    e.style.color = 'white';
+    e.style.padding = '30px';
+    e.style.textAlign = 'justify';
+    e.style.fontSize = '25px';
+}
+
+
